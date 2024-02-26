04c08b68809cd07983f96b0bea149a5ef638554c
mateusz-lisowski <mateusz.lisowski.dev@gmail.com>
2024-02-26 23:48:46 +0100

    Add api structure for iList

--- lab1/main.go
+++ lab1/main.go
@@ -3,6 +3,30 @@ package main
 
 import "fmt"
 
+type iNode struct {
+  value int
+  next *iNode
+}
+
+type iList struct {
+  first *iNode
+}
+
+func (il *iList) push(newVal int) {
+
+}
+
+func (il *iList) pop() int {
+  return 1
+}
+
+func (il *iList) swap(firstIndex, secondIndex int) {
+}
+
+
+func (il *iList) print() {
+
+}
 
 func main() {
   fmt.Println("xD1")
