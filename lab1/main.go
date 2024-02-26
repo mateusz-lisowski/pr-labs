0ae9bcf4046ab99eb54968a2394e3c044374d097
mateusz-lisowski <mateusz.lisowski.dev@gmail.com>
2024-02-27 00:21:06 +0100

    Add swap functionality

--- lab1/main.go
+++ lab1/main.go
@@ -1,46 +1,65 @@
 package main
 
-
-// import "fmt"
+import (
+	"errors"
+)
 
 type iNode struct {
-  value int
-  next *iNode
+	value int
+	next  *iNode
 }
 
 type iList struct {
-  first *iNode
+	first *iNode
 }
 
 func (il *iList) push(newVal int) {
-  var newNode iNode
-  newNode.value = newVal
-  newNode.next = il.first
-  il.first = &newNode
+	var newNode iNode
+	newNode.value = newVal
+	newNode.next = il.first
+	il.first = &newNode
 }
 
 func (il *iList) pop() int {
-  np := il.first
-  prev := np
-  for np.next != nil {
-    prev = np
-    np = np.next
-  }
-  prev.next = nil
-  return np.value
+	np := il.first
+	prev := np
+	for np.next != nil {
+		prev = np
+		np = np.next
+	}
+	prev.next = nil
+	return np.value
 }
 
-func (il *iList) swap(firstIndex, secondIndex int) {
-}
+func (il *iList) swap(firstIndex, secondIndex int) error {
+	firstNode := il.first
+	for i := 0; i < firstIndex; i++ {
+		if firstNode.next == nil {
+			return errors.New("Index out of range")
+		}
+		firstNode = firstNode.next
+	}
 
+	secondNode := il.first
+	for i := 0; i < secondIndex; i++ {
+		if secondNode.next == nil {
+			return errors.New("Index out of range")
+		}
+		secondNode = secondNode.next
+	}
+
+	firstNode.value, secondNode.value = secondNode.value, firstNode.value
+
+	return nil
+}
 
 func (il *iList) print() {
 
 }
 
 func main() {
-  var integerList iList
-  integerList.push(6)
-  integerList.push(0)
-  integerList.push(3)
+	var integerList iList
+	integerList.push(6)
+	integerList.push(0)
+	integerList.push(3)
 }
