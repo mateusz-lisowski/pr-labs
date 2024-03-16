c50d70672abc35d9dadb8e288657c30e06555576
mateusz-lisowski <mateusz.lisowski.dev@gmail.com>
2024-03-16 20:29:20 +0100

    Implement prime checking function

--- lab3/main.c
+++ lab3/main.c
@@ -4,7 +4,18 @@
 
 bool check_prime(int number)
 {
-  return false;
+	
+  if (number < 2)
+		return false;
+
+  for (int i = 2; i <= number / 2; i++) 
+  {
+    if (number % i == 0) 
+      return false;		
+  }
+  
+   return true; 
+
 }
 
 
